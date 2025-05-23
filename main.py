95eb5e2c4a89974cb652f0459c7ccf003d99ac8b
bszycher <borys.szycher.stud@pw.edu.pl>
2025-05-23 23:06:13 +0200

    commit

--- main.py
+++ main.py
@@ -7,130 +7,167 @@ from matplotlib.patches import Circle
 import matplotlib.animation as animation
 from matplotlib.widgets import Slider, Button
 
-class DoublePendulumApp:
-    def __init__(self):
-        # fizyka
-        self.L1, self.L2 = 1.0, 1.0
-        self.m1, self.m2 = 1.0, 1.0
-        self.g = 9.81
+# Parametry fizyczne domyślne
+g = 9.81
 
-        # parametry symulacji
-        self.dt = 0.01
-        self.tmax = 45.0
-        self.t = np.arange(0, self.tmax + self.dt, self.dt)
 
-        # parametry odtwarzania
-        self.playback_fps = 60
-        self.n_frames = int(self.tmax * self.playback_fps)
-        self.idx = None  # wypełni się później
+def prompt_initial_conditions():
+    """
+    Wyświetla okno z suwakami do ustawienia parametrów początkowych:
+    θ1, θ2 (kąty początkowe), L1, L2 (długości), m1, m2 (masy).
+    Zwraca wartości w tej kolejności.
+    """
+    # wartości inicjalne
+    init_vals = {
+        'theta1': 3 * np.pi / 7,
+        'theta2': 3 * np.pi / 4,
+        'L1': 1.0,
+        'L2': 1.0,
+        'm1': 1.0,
+        'm2': 1.0
+    }
 
-        # wartości startowe
-        self.theta1_0 = 3 * np.pi / 7
-        self.theta2_0 = 3 * np.pi / 4
+    fig, ax = plt.subplots(figsize=(8, 4))
+    plt.subplots_adjust(left=0.1, bottom=0.2)
+    ax.set_axis_off()
 
-        # kontenery na wynik
-        self.x1 = self.y1 = self.x2 = self.y2 = None
+    # suwaki
+    s_theta1 = Slider(plt.axes([0.1, 0.80, 0.8, 0.04]), 'θ₁ [rad]', 0, 2 * np.pi, valinit=init_vals['theta1'])
+    s_theta2 = Slider(plt.axes([0.1, 0.73, 0.8, 0.04]), 'θ₂ [rad]', 0, 2 * np.pi, valinit=init_vals['theta2'])
+    s_L1 = Slider(plt.axes([0.1, 0.66, 0.8, 0.04]), 'L₁ [m]', 0.1, 5.0, valinit=init_vals['L1'])
+    s_L2 = Slider(plt.axes([0.1, 0.59, 0.8, 0.04]), 'L₂ [m]', 0.1, 5.0, valinit=init_vals['L2'])
+    s_m1 = Slider(plt.axes([0.1, 0.52, 0.8, 0.04]), 'm₁ [kg]', 0.1, 10.0, valinit=init_vals['m1'])
+    s_m2 = Slider(plt.axes([0.1, 0.45, 0.8, 0.04]), 'm₂ [kg]', 0.1, 10.0, valinit=init_vals['m2'])
 
-    def equations(self, y, t):
-        θ1, z1, θ2, z2 = y
-        c, s = np.cos(θ1 - θ2), np.sin(θ1 - θ2)
-        dz1 = (self.m2 * self.g * np.sin(θ2) * c
-               - self.m2 * s * (self.L1 * z1 * z1 * c + self.L2 * z2 * z2)
-               - (self.m1 + self.m2) * self.g * np.sin(θ1)) \
-              / (self.L1 * (self.m1 + self.m2 * s * s))
-        dz2 = ((self.m1 + self.m2) * (self.L1 * z1 * z1 * s
-                                      - self.g * np.sin(θ2) + self.g * np.sin(θ1) * c)
-               + self.m2 * self.L2 * z2 * z2 * s * c) \
-              / (self.L2 * (self.m1 + self.m2 * s * s))
-        return [z1, dz1, z2, dz2]
+    btn = Button(plt.axes([0.4, 0.20, 0.2, 0.075]), 'Start')
 
-    def total_energy(self, Y):
-        θ1, θ1d, θ2, θ2d = Y.T
-        V = -(self.m1 + self.m2) * self.L1 * self.g * np.cos(θ1) \
-            - self.m2 * self.L2 * self.g * np.cos(θ2)
-        T = 0.5 * self.m1 * (self.L1 * θ1d) ** 2 \
-            + 0.5 * self.m2 * ((self.L1 * θ1d) ** 2 + (self.L2 * θ2d) ** 2
-                               + 2 * self.L1 * self.L2 * θ1d * θ2d * np.cos(θ1 - θ2))
-        return T + V
+    start_clicked = False
 
-    def prompt_initial_conditions(self):
-        fig, ax = plt.subplots(figsize=(10, 4))
-        plt.subplots_adjust(left=0.1, bottom=0.4)
-        ax.set_axis_off()
-        s1 = Slider(plt.axes([0.1, 0.25, 0.8, 0.03]),
-                    'θ₁ [rad]', 0, 2 * np.pi, valinit=self.theta1_0)
-        s2 = Slider(plt.axes([0.1, 0.15, 0.8, 0.03]),
-                    'θ₂ [rad]', 0, 2 * np.pi, valinit=self.theta2_0)
-        btn = Button(plt.axes([0.4, 0.05, 0.2, 0.075]), 'Start')
+    def on_start(evt):
+        nonlocal start_clicked
+        start_clicked = True
+        plt.close(fig)
 
-        def on_start(evt):
-            self.theta1_0, self.theta2_0 = s1.val, s2.val
-            plt.close(fig)
+    btn.on_clicked(on_start)
 
-        btn.on_clicked(on_start)
-        plt.show()
+    while plt.fignum_exists(fig.number) and not start_clicked:
+        plt.pause(0.1)
 
-    def run_simulation(self):
-        # integracja
-        y0 = [self.theta1_0, 0.0, self.theta2_0, 0.0]
-        Y = odeint(self.equations, y0, self.t)
-        # kontrola energii
-        E0 = self.total_energy(np.array(y0).reshape(1, 4))
-        if np.max(np.abs(self.total_energy(Y) - E0)) > 0.05:
-            sys.exit("Drift energii za duży")
-        # kartezjany
-        θ1, θ2 = Y[:, 0], Y[:, 2]
-        self.x1 = self.L1 * np.sin(θ1)
-        self.y1 = -self.L1 * np.cos(θ1)
-        self.x2 = self.x1 + self.L2 * np.sin(θ2)
-        self.y2 = self.y1 - self.L2 * np.cos(θ2)
+    if not start_clicked:
+        return None
 
-        # indeksy do 60 FPS
-        self.idx = np.linspace(0, len(self.t) - 1, self.n_frames).astype(int)
+    return (
+        s_theta1.val, s_theta2.val,
+        s_L1.val, s_L2.val,
+        s_m1.val, s_m2.val
+    )
 
-    def animate(self):
-        fig, ax = plt.subplots(figsize=(8, 6))
-        ax.set_xlim(-self.L1 - self.L2 - 0.1, self.L1 + self.L2 + 0.1)
-        ax.set_ylim(-self.L1 - self.L2 - 0.1, self.L1 + self.L2 + 0.1)
-        ax.set_aspect('equal')
-        plt.axis('off')
 
-        rod_line, = ax.plot([], [], lw=2, c='k')
-        trail_line, = ax.plot([], [], lw=1, alpha=0.5)
-        bob1 = Circle((0, 0), 0.05, fc='b', zorder=10)
-        bob2 = Circle((0, 0), 0.05, fc='r', zorder=10)
-        ax.add_patch(bob1)
-        ax.add_patch(bob2)
+def equations(y, t, L1, L2, m1, m2):
+    θ1, z1, θ2, z2 = y
+    c, s = np.cos(θ1 - θ2), np.sin(θ1 - θ2)
+    dz1 = (m2 * g * np.sin(θ2) * c
+           - m2 * s * (L1 * z1 * z1 * c + L2 * z2 * z2)
+           - (m1 + m2) * g * np.sin(θ1)) \
+          / (L1 * (m1 + m2 * s * s))
+    dz2 = ((m1 + m2) * (L1 * z1 * z1 * s
+                        - g * np.sin(θ2) + g * np.sin(θ1) * c)
+           + m2 * L2 * z2 * z2 * s * c) \
+          / (L2 * (m1 + m2 * s * s))
+    return [z1, dz1, z2, dz2]
 
-        def init():
-            rod_line.set_data([], [])
-            trail_line.set_data([], [])
-            bob1.center = (0, 0)
-            bob2.center = (0, 0)
-            return rod_line, trail_line, bob1, bob2
 
-        def update(frame):
-            i = self.idx[frame]
-            rod_line.set_data([0, self.x1[i], self.x2[i]],
-                              [0, self.y1[i], self.y2[i]])
-            bob1.center = (self.x1[i], self.y1[i])
-            bob2.center = (self.x2[i], self.y2[i])
-            tr0 = max(0, i - int(1.0 / self.dt))
-            trail_line.set_data(self.x2[tr0:i], self.y2[tr0:i])
-            return rod_line, trail_line, bob1, bob2
+def total_energy(Y, L1, L2, m1, m2):
+    θ1, θ1d, θ2, θ2d = Y.T
+    V = -(m1 + m2) * L1 * g * np.cos(θ1) - m2 * L2 * g * np.cos(θ2)
+    T = 0.5 * m1 * (L1 * θ1d) ** 2 + 0.5 * m2 * ((L1 * θ1d) ** 2 + (L2 * θ2d) ** 2
+                                                 + 2 * L1 * L2 * θ1d * θ2d * np.cos(θ1 - θ2))
+    return T + V
 
-        ani = animation.FuncAnimation(
-            fig, update, frames=self.n_frames,
-            init_func=init, blit=True,
-            interval=1000 / self.playback_fps
-        )
-        plt.show()
 
-    def run(self):
-        self.prompt_initial_conditions()
-        self.run_simulation()
-        self.animate()
+def run_simulation(theta1_0, theta2_0, L1, L2, m1, m2, dt=0.01, tmax=45.0, playback_fps=60):
+    t = np.arange(0, tmax + dt, dt)
+    y0 = [theta1_0, 0.0, theta2_0, 0.0]
+    Y = odeint(equations, y0, t, args=(L1, L2, m1, m2))
+
+    # kontrola energii
+    E0 = total_energy(np.array(y0).reshape(1, 4), L1, L2, m1, m2)
+    if np.max(np.abs(total_energy(Y, L1, L2, m1, m2) - E0)) > 0.05:
+        sys.exit("Drift energii za duży")
+
+    θ1, θ2 = Y[:, 0], Y[:, 2]
+    x1 = L1 * np.sin(θ1)
+    y1 = -L1 * np.cos(θ1)
+    x2 = x1 + L2 * np.sin(θ2)
+    y2 = y1 - L2 * np.cos(θ2)
+
+    n_frames = int(tmax * playback_fps)
+    idx = np.linspace(0, len(t) - 1, n_frames).astype(int)
+    return x1, y1, x2, y2, idx, L1, L2, playback_fps
+
+
+def animate(x1, y1, x2, y2, idx, L1, L2, playback_fps, trail_length=500):
+    fig, ax = plt.subplots(figsize=(8, 6))
+    ax.set_xlim(-L1 - L2 - 0.1, L1 + L2 + 0.1)
+    ax.set_ylim(-L1 - L2 - 0.1, L1 + L2 + 0.1)
+    ax.set_aspect('equal')
+    plt.axis('off')
+
+    rod_line, = ax.plot([], [], lw=2, color='black')  # Grubsza linia wahadła
+    trail_line, = ax.plot([], [], lw=1, color='red', alpha=0.7)  # Czerwony ślad
+
+    bob1 = Circle((0, 0), 0.05, fc='b', zorder=10)
+    bob2 = Circle((0, 0), 0.05, fc='r', zorder=10)
+    ax.add_patch(bob1)
+    ax.add_patch(bob2)
+
+    # Inicjalizacja danych śladu
+    trail_x = []
+    trail_y = []
+
+    def init():
+        rod_line.set_data([], [])
+        trail_line.set_data([], [])
+        bob1.center = (0, 0)
+        bob2.center = (0, 0)
+        return rod_line, trail_line, bob1, bob2
+
+    def update(frame):
+        nonlocal trail_x, trail_y
+
+        i = idx[frame]
+        rod_line.set_data([0, x1[i], x2[i]], [0, y1[i], y2[i]])
+        bob1.center = (x1[i], y1[i])
+        bob2.center = (x2[i], y2[i])
+
+        # Aktualizacja śladu
+        trail_x.append(x2[i])
+        trail_y.append(y2[i])
+
+        # Ogranicz długość śladu
+        if len(trail_x) > trail_length:
+            trail_x = trail_x[-trail_length:]
+            trail_y = trail_y[-trail_length:]
+
+        # Aktualizacja wizualizacji śladu
+        trail_line.set_data(trail_x, trail_y)
+
+        return rod_line, trail_line, bob1, bob2
+
+    ani = animation.FuncAnimation(
+        fig, update, frames=len(idx), init_func=init,
+        blit=True, interval=1000 / playback_fps
+    )
+    plt.show()
 
 
 if __name__ == '__main__':
-    DoublePendulumApp().run()
+    initial_conditions = prompt_initial_conditions()
+    if initial_conditions is None:
+        sys.exit("Anulowano symulację (zamknięto okno).")
+
+    theta1_0, theta2_0, L1, L2, m1, m2 = initial_conditions
+    x1, y1, x2, y2, idx, L1, L2, fps = run_simulation(
+        theta1_0, theta2_0, L1, L2, m1, m2
+    )
+    animate(x1, y1, x2, y2, idx, L1, L2, fps)
